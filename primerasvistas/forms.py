e1d954efd862a50b2889b0923d71e6372b12a77b
Mic1286 <michelle-m@live.com.ar>
2022-07-24 17:33:00 -0300

    hice cambios

--- primerasvistas/forms.py
+++ primerasvistas/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class FormCelulares(forms.Form):
     marca = forms.CharField(max_length=20)
     modelo = forms.CharField(max_length=50)
     fecha_registro = forms.DateField(required=False)
+    
     
 class BusquedaCelulares(forms.Form):
     nombre = forms.CharField(max_length=30, required=False)
